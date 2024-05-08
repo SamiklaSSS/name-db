053e7fca9321efede423bfc6d3623e4015dd57b2
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2024-05-08 10:15:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,30 +51,59 @@ while True:
             print("Name found:")
             for row in result:
                 print(row)
+
+
+
     elif choice == '3':
         # atlasīt top 5 vārdus pēc populāritātes
         # https://www.w3schools.com/sql/sql_orderby.asp (ORDER BY)
         # https://www.w3schools.com/sql/sql_top.asp (LIMIT)
+        limit = 5
+        cursor.execute("SELECT * FROM names ORDER BY amount LIMIT", (limit,))
+        result = cursor.fetchall()
         pass
+
+
+
     elif choice == '4':
         gender = input("Ievadi VĪRIETIS/SIEVIETE: ")
         # Parādīt kopējo vārdu skaitu pēc dzimuma
         # https://www.w3schools.com/sql/sql_count.asp (COUNT)
         # https://www.w3schools.com/sql/sql_where.asp (WHERE)
+        cursor.execute("SELECT COUNT (*) FROM names WHERE gender=?", (gender))
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+
+
     elif choice == '5':
         gender = input("Ievadi VĪRIETIS/SIEVIETE: ")
         # Parādīt vidējo vārdu skaitu pēc dzimuma
         # https://www.w3schools.com/sql/sql_avg.asp (AVG)
         # https://www.w3schools.com/sql/sql_where.asp (WHERE)
+        #-???
+
+
+
     elif choice == '6':
         amount = input("Ievadi daudzumu: ")
         # Parādīt vārdus pēc daudzuma
         # https://www.w3schools.com/sql/sql_where.asp (WHERE)
+        cursor.execute("SELECT * FROM names WHERE amount=?", (amount))
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+
+
     elif choice == '7':
         break
     else:
         print("Invalid choice. Please enter a valid option.")
 
 
+# Close connection
+conn.close()
 # Close connection
 conn.close()
